80862d5208fec718b31ac707a61cabd708957a74
홍진석 <hjs@hongjinseog-ui-MacBookPro.local>
2024-12-15 01:25:23 +0900

    fix: lusitina to lusitana

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Image from 'next/image';
 import Link from 'next/link';
 import AcmeLogo from './ui/acme-logo';
-import { lusitina } from './ui/font';
+import { lusitana } from './ui/font';
 
 export default function Page() {
   return (
@@ -13,7 +13,7 @@ export default function Page() {
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
           <p
-            className={`${lusitina.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}
+            className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}
           >
             <strong>Welcome to Acme.</strong> This is the example for the{' '}
             <a href="https://nextjs.org/learn/" className="text-blue-500">
